96e4aa02d3159168b508c3d9fd00d83c2e0d2e88
Franco <fat7_2014@hotmail.com>
2022-11-18 19:37:09 -0300

    Agrego proyecto boletines

---
+++ boletines/main.js
@@ -0,0 +1,23 @@
+/*
+    let links = document.querySelectorAll(".close");
+
+    links.forEach(function(link){
+
+    link.addEventListener("click",function(ev){
+    ev.preventDefault();
+
+let contenido = document.querySelector('.contenido');
+
+contenido.classList.remove('animate__animated', 'animate__bounceIn');
+
+contenido.classList.add('animate__animated', 'animate__bounceOut');
+
+
+return false;
+
+setTimeout(function)
+
+location.href = "/";
+}) 
+}):
+*/
